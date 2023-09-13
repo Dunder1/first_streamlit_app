fc499e7e6301c9b68803d599c0eb0e0eecef5974
Dunder1 <132176663+Dunder1@users.noreply.github.com>
2023-09-13 11:52:36 +0800

    Update streamlit_app.py
    make multiselect by fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
@@ -10,7 +13,5 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
-
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
