f9d78b8531244e1cd781df0ed3604f58063d844c
Dunder1 <132176663+Dunder1@users.noreply.github.com>
2023-09-14 09:12:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -38,8 +41,8 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-st.header("the fruit load list contains:")
-st.dataframe(my_data_rows)
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#st.header("the fruit load list contains:")
+#st.dataframe(my_data_rows)
 
